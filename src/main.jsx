36c4d110b036beb74d3b335105dae16b09adbf0b
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-08-19 21:20:22 +0300

    321

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "/src/components/App/App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "modern-normalize";
+import App from "./components/App/App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
